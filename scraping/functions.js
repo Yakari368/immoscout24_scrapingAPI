c951e0cd3d1294a1cb3b65e0dd71d64bb722df17
imadghazaz <imad3002@gmail.com>
2024-10-19 17:44:01 +0100

    image fetching edit

--- scraping/functions.js
+++ scraping/functions.js
@@ -13,49 +13,60 @@ async function close_popup(page) {
 
 
 async function getData(page) {
-    const resultList = await page.evaluate(() => {
-        const result = window.IS24 ? window.IS24.resultList.resultListModel.searchResponseModel : null;
-        return result["resultlist.resultlist"].resultlistEntries[0].resultlistEntry
-    });
-    let resultObject = [];
-    resultList.map((result, index) => {
-        if (result['resultlist.realEstate'].privateOffer == 'true') {
-            let gallery=[];
-            result['resultlist.realEstate'].galleryAttachments.attachment.map(image=>{
-                image = image.urls[0].url["@href"]
-                const endIndex = image.indexOf('ORIG');
-                gallery.push(image.substring(0, endIndex).trim())
-            })
-            
-            
+    try {
+        const resultList = await page.evaluate(() => {
+            const result = window.IS24 ? window.IS24.resultList.resultListModel.searchResponseModel : null;
+            return result["resultlist.resultlist"].resultlistEntries[0].resultlistEntry
+        });
+        let resultObject = [];
+        resultList.map((result, index) => {
+            if (result['resultlist.realEstate'].privateOffer == 'true') {
+                let gallery = [];
+                const attachment = result['resultlist.realEstate'].galleryAttachments.attachment
+                if (Array.isArray(attachment)) {
+                    attachment.map(image => {
+                        image = image.urls[0].url["@href"]
+                        const endIndex = image.indexOf('ORIG');
+                        gallery.push(image.substring(0, endIndex).trim())
 
-            resultObject.push(
-                {
-                    id: index,
-                    title: result['resultlist.realEstate'].title,
-                    address: result['resultlist.realEstate'].address,
-                    contactDetails:result['resultlist.realEstate'].contactDetails,
-                    gallery,
-                    price: result['resultlist.realEstate'].price,
-                    livingSpace: result['resultlist.realEstate'].livingSpace + 'm²',
-                    numberOfRooms: result['resultlist.realEstate'].numberOfRooms ,
-                    constructionYear: result['resultlist.realEstate'].constructionYear,
-                    estateInfo:{
-                        builtInKitchen: result['resultlist.realEstate'].builtInKitchen,
-                        balcony: result['resultlist.realEstate'].balcony,
-                        garden: result['resultlist.realEstate'].garden,
-                        hasCourtage: result['resultlist.realEstate'].courtage.hasCourtage,
-                        lift: result['resultlist.realEstate'].lift,
-                        guestToilet: result['resultlist.realEstate'].guestToilet,
-                        cellar: result['resultlist.realEstate'].cellar,
-                        isBarrierFree: result['resultlist.realEstate'].isBarrierFree,
-                    },
-                    monthlyRate:result['resultlist.realEstate'].monthlyRate,
-                    modificationDate: result["@modification"],
-                })
-        }
-    })
-    return resultObject
+                    })
+                } else if (attachment.urls) {
+                    const image = attachment.urls[0].url["@href"]
+                    const endIndex = image.indexOf('ORIG');
+                    gallery.push(image.substring(0, endIndex).trim())
+                }
+
+                resultObject.push(
+                    {
+                        id: index,
+                        title: result['resultlist.realEstate'].title,
+                        address: result['resultlist.realEstate'].address,
+                        contactDetails: result['resultlist.realEstate'].contactDetails,
+                        gallery,
+                        price: result['resultlist.realEstate'].price,
+                        livingSpace: result['resultlist.realEstate'].livingSpace + 'm²',
+                        numberOfRooms: result['resultlist.realEstate'].numberOfRooms,
+                        constructionYear: result['resultlist.realEstate'].constructionYear,
+                        estateInfo: {
+                            builtInKitchen: result['resultlist.realEstate'].builtInKitchen,
+                            balcony: result['resultlist.realEstate'].balcony,
+                            garden: result['resultlist.realEstate'].garden,
+                            hasCourtage: result['resultlist.realEstate'].courtage.hasCourtage,
+                            lift: result['resultlist.realEstate'].lift,
+                            guestToilet: result['resultlist.realEstate'].guestToilet,
+                            cellar: result['resultlist.realEstate'].cellar,
+                            isBarrierFree: result['resultlist.realEstate'].isBarrierFree,
+                        },
+                        monthlyRate: result['resultlist.realEstate'].monthlyRate,
+                        modificationDate: result["@modification"],
+                    })
+            }
+        })
+        return resultObject
+    } catch (error) {
+        return new Error(error)
+    }
+
 }
 
-module.exports = {getData,close_popup}
+module.exports = { getData, close_popup }
