2fc02bf0ca0588b2d4062a2923dcf0c985ed789f
imadghazaz <imad3002@gmail.com>
2024-10-25 09:06:54 +0100

    remove logs

--- scraping/main.js
+++ scraping/main.js
@@ -1,21 +1,22 @@
 const puppeteer = require("puppeteer-core");
-const { close_popup,getData }=require("./functions");
+const { close_popup, getData } = require("./functions");
 
-async function scrape(url,BROWSER_WS) {
-    console.log("Connecting to browser...");
+async function scrape(url, BROWSER_WS) {
+    // console.log("Connecting to browser...");
     const browser = await puppeteer.connect({
         browserWSEndpoint: BROWSER_WS,
     });
-    console.log("Connected! Navigate to site...");
+    // console.log("Connected! Navigate to site...");
     const page = await browser.newPage();
     await page.goto(url, { waitUntil: "networkidle2", timeout: 60000 });
-    console.log("Navigated! Waiting for popup...");
+    // console.log("Navigated! Waiting for popup...");
     await close_popup(page);
-    console.log("Extracting data...");
+    // console.log("Extracting data...");
     const JSONResult = await getData(page)
-    console.log("data extracted! closing browser ...");
+    // console.log("data extracted! closing browser ...");
     await browser.close();
-    return JSONResult
+    if (JSONResult.code == 501) throw new Error(JSONResult.error);
+    else return JSONResult.resultObject
 }
 
-module.exports = {scrape}
+module.exports = { scrape }
