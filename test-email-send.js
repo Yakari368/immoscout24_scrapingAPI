dc622842567e6bee1365ebb86c25427429055e37
Milton Correia <milseg.dev@gmail.com>
2026-03-04 16:22:05 -0300

    fix: email sending

--- test-email-send.js
+++ test-email-send.js
@@ -1,10 +1,46 @@
 const nodemailer = require('nodemailer');
+const FormData = require("form-data"); // form-data v4.0.1
+const Mailgun = require("mailgun.js"); // mailgun.js v11.1.0
+
 
 const SMTP_USER = process.env.SMTP_USER || 'test@test.com';
 const SMTP_PASSWORD = process.env.SMTP_PASSWORD || '';
 const SMTP_PORT = process.env.SMTP_PORT || '465';
 const SMTP_SECURE = process.env.SMTP_SECURE || 'off';
 const SMTP_HOST = process.env.SMTP_HOST || ''
+const MAILGUN_API_KEY = process.env.MAILGUN_API_KEY || '';
+const MAILGUN_DOMAIN = process.env.MAILGUN_DOMAIN || '';
+
+const mailgun = new Mailgun(FormData);
+const mg = mailgun.client({
+    username: "api",
+    key: MAILGUN_API_KEY,
+    // When you have an EU-domain, you must specify the endpoint:
+    // url: "https://api.eu.mailgun.net"
+});
+
+const sendMailMailgun = async function ({ to, from, replyTo, subject, text, html }) {
+  try {
+    let mailOptions = {
+      from: from,
+      to: to,
+      subject,
+      text,
+      html: html,
+      'h:Content-Language': 'de-DE',
+    };
+    if (replyTo) {
+        mailOptions['h:Reply-To'] = replyTo;
+    }
+    const data = await mg.messages.create(MAILGUN_DOMAIN, mailOptions);
+
+    console.log(data); // logs response data
+    return { success: true, messageId: JSON.stringify(data) };
+  } catch (error) {
+    console.log(error); //logs any error
+    return { success: false, error: error };
+  }
+};
 
 const transporter = nodemailer.createTransport({
     host: SMTP_HOST,
@@ -61,14 +97,22 @@ async function sendEmailInternal({ to, from, replyTo, subject, text, html }) {
 module.exports.sendTestEmail = async ({subject, text, html, replyTo}) => {
     let ret = {success: false, error: "noninitialized"}
     try {
-        ret = await sendEmailInternal({
-            to: ['milton.segundo@aiautomationonline.de', 'kevin@aiautomationonline.de'],
-            from: SMTP_USER,
-            replyTo: [replyTo],
-            subject,
-            text,
-            html,
-        })
+        ret = await sendMailMailgun({
+                to: ['milton.segundo@aiautomationonline.de', 'kevin@aiautomationonline.de'],
+                from: `Immoscout Setup <${MAILGUN_FROM}>`,
+                replyTo: [replyTo],
+                subject,
+                text,
+                html,
+            })
+        // ret = await sendEmailInternal({
+        //     to: ['milton.segundo@aiautomationonline.de', 'kevin@aiautomationonline.de'],
+        //     from: SMTP_USER,
+        //     replyTo: [replyTo],
+        //     subject,
+        //     text,
+        //     html,
+        // })
     } catch (err) {
         console.log(`Error sending test email 1 ${subject} ${text} ${replyTo}`)
         return 'failed'
